143a10ba5d60f14a4106630f4356eeab89fa3cd0
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-12-06 13:32:36 +0100

    nummern ohne satzzeichen

--- javascript-object-car-enza.js
+++ javascript-object-car-enza.js
@@ -3,7 +3,7 @@ const meinAuto = {
     marke: "Ford" , 
     modell: "F"+150 , 
     baujahr: 2017 , 
-    kilometer: 110+"."+"000" , 
+    kilometer: 110000 , 
     motor: "Benziner" ,
 }
 
@@ -13,7 +13,7 @@ console.log("-------------");
 //Aufgabenstellung - Teil 2
 
 meinAuto.baujahr = 2019;
-meinAuto.kilometer = 30+"."+"000";
+meinAuto.kilometer = 30000;
 meinAuto.motor = "Diesel";
 
 console.log(meinAuto);
