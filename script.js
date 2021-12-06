41316666b6a6e50a7bcbb583fead76a203bbbb97
sham912411 <shamalhakim91@gmail.com>
2021-12-06 13:54:23 +0100

    lösung

---
+++ script.js
@@ -0,0 +1,13 @@
+const meinAuto = {
+    Marke: "Ford",
+    Modell: "F150",
+    Baujahr: "2017",
+    Kilometer: "110000",
+    Motor: "Benziner"
+};
+meinAuto.Baujahr = "2019";
+meinAuto.Kilometer = "30000";
+meinAuto.Motor = "Diesel";
+
+console.log("Alte Werte: ", meinAuto);
+console.log("Neue Werte: ", meinAuto);
