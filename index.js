2ebd316a7a374b6510666d4fc583f99f53853ff8
aisha2000-hub <aisha.noori2000@gmail.com>
2021-12-06 13:36:13 +0100

    Die LÖsung von Aisha

---
+++ index.js
@@ -0,0 +1,28 @@
+// TEIL 1
+const meinAuto ={
+    Marke: "Ford",
+    Modell: "F150",
+    Baujahr: 2017,
+    Kilometer:110000,
+    Motor: "Benziner",
+}
+console.log("Marke:" ,meinAuto. Marke)
+console.log("Modell:" ,meinAuto. Modell)
+console.log("Baujahr:" ,meinAuto. Baujahr );
+console.log("Kilometer:" ,meinAuto. Kilometer);
+console.log("Motor:" ,meinAuto. Motor);
+
+
+// TEIL2
+meinAuto.Baujahr = 2019;
+meinAuto.Kilometer = 30000;
+meinAuto.Motor = "Diesel";
+
+
+// TEIL 3
+console.log("Marke:" ,meinAuto. Marke)
+console.log("Modell:" ,meinAuto. Modell)
+console.log("Baujahr:" ,meinAuto. Baujahr );
+console.log("Kilometer:" ,meinAuto. Kilometer);
+console.log("Motor:" ,meinAuto. Motor);
+
