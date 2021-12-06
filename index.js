3de37ad60664c4e26f5d0eb734278af134e25e9f
sprotte <metatron1972@live.de>
2021-12-06 13:40:04 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const meinAuto = {
-marke = "Ford",
-modell = "F150",  
-baujahr = 2017,
-kilometer = 110000,
-motor = "Benziener"
+marke: "Ford",
+modell: "F150",  
+baujahr: 2017,
+kilometer: 110000,
+motor: "Benziener"
 };
+console.log(meinAuto);
+meinAuto.baujahr = 2019;
+meinAuto.kilometer = 30000;
+meinAuto.motor = "Diesel";
+
+
+console.log(meinAuto);
+console.log("Das Baujahr ist:",meinAuto.baujahr);
+console.log("Kilometerstand",meinAuto.kilometer);
+console.log("Was trinkt er:",meinAuto.motor);
+
+
