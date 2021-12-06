2d2f2804f2050d9d6c336f8412f3878cad808e64
Salimov333 <salimov333@gmail.com>
2021-12-06 13:23:02 +0100

    Salem Lösung

---
+++ index.js
@@ -0,0 +1,14 @@
+const meinAuto = {
+    Marke: "Ford",
+    Modell: "F150",
+    Baujahr: 2017,
+    Kilometer: 110000,
+    Motor: "Benziner"
+}
+console.log("Version1: ", meinAuto);
+
+meinAuto.Baujahr = 2019;
+meinAuto.Kilometer = 30000;
+meinAuto.Motor = "Diesel";
+
+console.log("Version2: ", meinAuto); // Die Werte wurden angepasst
