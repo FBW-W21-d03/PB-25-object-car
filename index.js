5842547501ee254a7902fea3aa13a4a6deac6076
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-12-06 13:26:00 +0100

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+const meinAuto = {
+    Marke: "Ford",
+    Modell:"F150" ,
+    Baujahr: 2017,
+    Kilometer:110.000,
+    Motor:"Benziner",
+};
+meinAuto.Baujahr =2019;
+meinAuto.Kilometer = 110.000;
+meinAuto.Motor = "Diesel";
+console.log(meinAuto);
