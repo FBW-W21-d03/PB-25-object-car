f1eb45c305b7a9daf86c52bb9ab3754672843542
Dennisbullys <d.rochus@hotmail.de>
2021-12-06 13:22:38 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,35 @@
+// # javascript-object-car
+
+// Aufgabenstellung - Teil 1
+
+const meinAuto = {
+    marke : "Ford",
+    modell : "f150",
+    baujahr : 2017,
+    kilometer : 110.000,
+    motor : "Benziner"
+}
+
+
+// Aufgabenstellung - Teil 2
+
+
+meinAuto.baujahr =2019;
+meinAuto.kilometer=30.000;
+meinAuto.motor="Diesel";
+
+console.log(meinAuto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
