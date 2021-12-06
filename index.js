50a0ecbfd36dd97cc324e2ace7f49290df88d2a9
Serdar68 <Serdarbalkis@outlook.de>
2021-12-06 13:49:02 +0100

    update

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const meinAuto = {
     Marke: "Ford",
     Modell: "F150",
     Baujahr: "2017",
-    Kilometer: "110.000",
+    Kilometer: "110000",
     Motor: "Benziner"
-}
+};
+
+console.log("Alte Werte: ", meinAuto);
 
 //aktualisirung der daten
-meinAuto.Baujahr = "2019"
-meinAuto.Kilometer = "30.000"
-meinAuto.Motor = "Diesel"
+meinAuto.Baujahr = "2019";
+meinAuto.Kilometer = "30000";
+meinAuto.Motor = "Diesel";
 
 //Ausgabe
-console.log(meinAuto)
+console.log("Neue Werte: ", meinAuto);
