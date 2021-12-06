2cb4b1d82a4f93e5246366b91b576b5d773d7bfe
slavablack <veaceslav.ciornii@magenta.de>
2021-12-06 13:30:37 +0100

    Veaceslav Auto

---
+++ index.js
@@ -0,0 +1,14 @@
+//1
+const meinAuto = {
+    Marke: "Ford",
+    Modell: "F150",
+    Baujahr: 2017,
+    Kilometerstand: 110000,
+    Motor: "Benziner"
+}
+console.log(meinAuto);
+//2
+meinAuto.Baujahr=2019;
+meinAuto.Kilometerstand=30000;
+meinAuto.Motor="Diesel"
+console.log(meinAuto);
