d1617c89abc36193938d24bc3bbcfb4da8053853
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-12-06 13:29:58 +0100

    so?

---
+++ javascript-object-car-enza.js
@@ -0,0 +1,23 @@
+//Aufgabenstellung - Teil 1
+const meinAuto = {
+    marke: "Ford" , 
+    modell: "F"+150 , 
+    baujahr: 2017 , 
+    kilometer: 110+"."+"000" , 
+    motor: "Benziner" ,
+}
+
+console.log(meinAuto);
+console.log("-------------");
+
+//Aufgabenstellung - Teil 2
+
+meinAuto.baujahr = 2019;
+meinAuto.kilometer = 30+"."+"000";
+meinAuto.motor = "Diesel";
+
+console.log(meinAuto);
+
+
+
+
