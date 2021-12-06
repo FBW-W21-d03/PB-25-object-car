405e3a367de98346db3d9170e2eaf3d6cb174364
Serdar68 <Serdarbalkis@outlook.de>
2021-12-06 13:27:12 +0100

    serdar-aktualisierung-array

---
+++ index.js
@@ -0,0 +1,17 @@
+
+//Deklarieren
+const meinAuto = {
+    Marke: "Ford",
+    Modell: "F150",
+    Baujahr: "2017",
+    Kilometer: "110.000",
+    Motor: "Benziner"
+}
+
+//aktualisirung der daten
+meinAuto.Baujahr = "2019"
+meinAuto.Kilometer = "30.000"
+meinAuto.Motor = "Diesel"
+
+//Ausgabe
+console.log(meinAuto)
