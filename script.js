bc40b911424ffa49447c8eecd99b3c058b2a10cf
Vens <v.kolev@alfatrex.com>
2021-12-06 13:32:03 +0100

    vens

---
+++ script.js
@@ -0,0 +1,13 @@
+const meinAuto = {
+    marke: "Ford",
+    modell: "F150",
+    baujahr: "2017",
+    kilometer: "110 000",
+    motor: "Benziner"
+}
+
+meinAuto.baujahr = "2019";
+meinAuto.kilometer = "30 000";
+meinAuto.motor = "Diesel";
+
+console.log(meinAuto);
