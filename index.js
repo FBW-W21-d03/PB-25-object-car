c01f6c48414786711860d7fc489ac0714511e10f
H94ssein44 <alali.52@yahoo.com>
2021-12-06 13:32:20 +0100

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const meinAuto = {
+
+Marke: "Ford",
+Modell: "F150",
+Baujahr: 2017,
+Kilometer: 110000,
+Motor: "Benziner",
+};
+
+
+meinAuto.Baujahr = 2019;
+
+meinAuto. Kilometer = 30.000;
+
+meinAuto. Motor = "Diesel";
+
+console.log  (meinAuto)
+
