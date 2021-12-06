dce5702a7215efb497cd56e150c80442e638bb0e
Mak513 <osei.makangu@hotmail.com>
2021-12-06 13:31:27 +0100

    osei

---
+++ index.js
@@ -0,0 +1,15 @@
+const auto = {
+    Marke: "Ford",
+    Modell: "F150",
+    Baujahr: 2017,
+    Kilometer: 110.000,
+    Motor: "Benziner"
+};
+
+console.log(auto);
+
+ auto.Baujahr = 2021;
+ auto.Kilometer = 30.000;
+ auto.Motor = "Diesel"
+
+ console.log(auto);
