72bc9acc3d0a739dcc177be68adccbe521219b70
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-12-06 13:25:28 +0100

    Orhan's lösung

---
+++ object-car.js
@@ -0,0 +1,13 @@
+const meineAuto = {
+    marke: 'Ford',
+    model: 'F150',
+    baujahr: '2017',
+    kilometer: '110 000 kl',
+    motor: 'Benziner'
+}
+
+meineAuto.baujahr = '2019';
+meineAuto.kilometer = '30 000 kl';
+meineAuto.motor = 'Diesel';
+
+console.log(meineAuto);
