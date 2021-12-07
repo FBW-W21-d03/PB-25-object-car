12e51aed0249791bbcb34afb332fddc7f162457d
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-12-07 09:11:10 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,17 @@
+const meinAuto = {
+    marke: 'Ford',
+    modell: 'F150',
+    baujahr: '2017',
+    kilometer:'110000',
+    motor: 'Benziner'
+};
+
+console.log(meinAuto);
+
+
+meinAuto.baujahr= '2019';
+meinAuto.kilometer='30000';
+meinAuto.motor = 'Diesel';
+
+
+console.log(meinAuto);
